a7464fdf891e51f76afdb374cb9dbcfffa6e6cd9
easyten/yan-saraev-4 <yansaraev@gmail.com>
2014-04-07 20:10:56 +0600

    Initial Commit

---
+++ StackOverflow-ReceiveEvents/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StackOverflow-ReceiveEvents
+//
+//  Created by Yan Saraev on 4/7/14.
+//  Copyright (c) 2014 NeilGaliaskarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
